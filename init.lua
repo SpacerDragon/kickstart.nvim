afb81126967011552d73f095a9ff5e25d41ae1df
Per Idar <post@peridar.net>
2025-10-16 05:31:21 +0200

    Updated my init.lua to a working one

--- init.lua
+++ init.lua
@@ -133,16 +133,13 @@ require('lazy').setup({
     },
   },
 
+
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+      'lukas-reineke/indent-blankline.nvim',
+      main = 'ibl',  -- new entry point
   },
 
+  
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
@@ -231,10 +228,11 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
-
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Exit insert mode quickly' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -288,7 +286,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -438,15 +436,16 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+-- Modern replacement for lspconfig (Neovim ≥ 0.11)
+local lspconfig = vim.lsp.config  -- ✅ new API
+
+for server_name, server_settings in pairs(servers) do
+  lspconfig[server_name] = {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = server_settings,
+  }
+end
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
